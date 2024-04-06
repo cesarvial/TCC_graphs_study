938b64a53ac7ffcaa80c49a21df41314ba2988e9
Pieraqua <pieraquara@gmail.com>
2024-04-06 19:05:06 -0300

    Análise do grafo bipartido

---
+++ exploracao_dados/data_processing.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+dados_formados_path = 'input/dados_formados.csv'
+dados_regulares_path = 'input/dados_regulares.csv'
+dados_trancados_path = 'input/dados_trancados.csv'
+dados_desistentes_path = 'input/dados_desistentes.csv'
+
+
+def load_student_data() -> dict:
+    dados = {
+        "formados": pd.read_csv(dados_formados_path, delimiter=';'),
+        "regulares": pd.read_csv(dados_regulares_path, delimiter=';'),
+        "desistentes": pd.read_csv(dados_desistentes_path, delimiter=';'),
+        "trancados": pd.read_csv(dados_trancados_path, delimiter=';')
+    }
+
+    dados.update(todos = pd.concat([dados["desistentes"], dados["regulares"], dados["formados"], dados["trancados"]]))
+    return dados
+
+
+disciplinas = []
+with open('input/dependencias.txt') as f:
+    for line in f:
+        curr = line.split(';')
+        if 'P8' in curr:
+            curr = curr[1].split('\n')[0]
+            disciplinas.append(curr)
+
+disciplinas_obrigatorias = ['GE70D', 'EEC31', 'CSS30','EEX23']
+optativas = np.array(disciplinas)
+optativas = optativas[[disc not in disciplinas_obrigatorias for disc in optativas]]
+
+def get_optativas() -> list:
+
+    return optativas
