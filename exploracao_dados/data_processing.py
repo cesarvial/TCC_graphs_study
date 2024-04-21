895d5d9f9e5a80fd63fbe3ac3c7875128f68342f
pieraqua <pauloffxii@hotmail.com>
2024-04-21 19:23:15 -0300

    Inicio do desenvolvimento do pós-processamento

--- exploracao_dados/data_processing.py
+++ exploracao_dados/data_processing.py
@@ -22,6 +22,10 @@ def load_student_data() -> dict:
     dados['formados'] = dados['formados'].dropna()
     dados['trancados'] = dados['trancados'].dropna()
     dados['todos'] = dados['todos'].dropna()
+    
+    for item in dados:
+        dados[item]['CR'] = dados[item]['CR'].apply(lambda x: float(str(x).replace(',','.')))
+        dados[item]['NOTA'] = dados[item]['NOTA'].apply(lambda x: float(str(x).replace(',','.')))
 
     return dados
 
@@ -41,3 +45,48 @@ optativas = optativas[[disc not in disciplinas_obrigatorias for disc in optativa
 def get_optativas() -> list:
 
     return optativas
+
+from collections import defaultdict
+from typing import List
+import re
+
+class Disciplina:
+    def __init__(self, ch : int = 0, periodo : int = 0, nome : str = 'default', codigo : str = 'default', trilha : str = 'optativa'):
+        self.ch = ch
+        self.periodo = periodo
+        self.nome = nome
+        self.codigo = codigo
+        self.trilha = trilha
+
+def load_disciplinas() -> List:
+    disciplinas = defaultdict()
+
+    data = load_student_data() 
+    with open('input/dependencias.txt', 'r') as file:
+        for line in file:
+            entry = line.split(';')
+            if re.search('P[1-9]', entry[0]) != None and entry[1].split('\n')[0] not in disciplinas:
+                disc_code = entry[1].split('\n')[0]
+                disc_CH = data['todos'][data['todos']['CODIGO'] == disc_code].values[0][8] if len(data['todos'][data['todos']['CODIGO'] == disc_code].values) > 0 else 0
+                disc_periodo = int(entry[0][1].split('\n')[0])
+                disciplinas[entry[1].split('\n')[0]] = Disciplina(ch = int(disc_CH), periodo = disc_periodo, nome = entry[1].split('\n')[0], codigo = entry[1].split('\n')[0], trilha = 'optativa' if disc_periodo != 10 else 'obrigatoria')
+            elif re.search('P[1-9]', entry[1]) != None:
+                disc_code = entry[0]
+                disc_CH = data['todos'][data['todos']['CODIGO'] == disc_code].values[0][8] if len(data['todos'][data['todos']['CODIGO'] == disc_code].values) > 0 else 0
+                disc_periodo = int(entry[1][1].split('\n')[0]) - 1
+                disciplinas[entry[0].split('\n')[0]] = Disciplina(ch = int(disc_CH), periodo = disc_periodo, nome = entry[1].split('\n')[0], codigo = entry[1].split('\n')[0], trilha = 'obrigatoria')
+
+
+    with open('input/trilhas.txt', 'r') as file:
+        for line in file:
+            trilha = line.split('\n')[0].split(';')
+            for item in trilha[1:]:
+                if item in disciplinas:
+                    disciplinas[item].trilha = trilha[0]
+                else:
+                    disc_code = item
+                    disc_CH = data['todos'][data['todos']['CODIGO'] == disc_code].values[0][8] if len(data['todos'][data['todos']['CODIGO'] == disc_code].values) > 0 else 0
+                    disc_periodo = 8
+                    disciplinas[disc_code] = Disciplina(ch = int(disc_CH), periodo = disc_periodo, nome = item, codigo = item)
+
+    return disciplinas
