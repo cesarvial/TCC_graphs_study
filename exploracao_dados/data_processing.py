db56387e189b0925af171b82a5a2a91b747cd8b3
pieraqua <pauloffxii@hotmail.com>
2024-05-06 19:46:42 -0300

    Pos processamento guloso

--- exploracao_dados/data_processing.py
+++ exploracao_dados/data_processing.py
@@ -26,6 +26,7 @@ def load_student_data() -> dict:
     for item in dados:
         dados[item]['CR'] = dados[item]['CR'].apply(lambda x: float(str(x).replace(',','.')))
         dados[item]['NOTA'] = dados[item]['NOTA'].apply(lambda x: float(str(x).replace(',','.')))
+        dados[item]['ID_ANONIMO'] = dados[item]['ID_ANONIMO'].apply(lambda x: float(str(x).replace(',','.')))
 
     return dados
 
@@ -51,12 +52,13 @@ from typing import List
 import re
 
 class Disciplina:
-    def __init__(self, ch : int = 0, periodo : int = 0, nome : str = 'default', codigo : str = 'default', trilha : str = 'optativa'):
+    def __init__(self, ch : int = 0, periodo : int = 0, nome : str = 'default', codigo : str = 'default', trilha : str = 'optativa', equivalentes = []):
         self.ch = ch
         self.periodo = periodo
         self.nome = nome
         self.codigo = codigo
         self.trilha = trilha
+        self.equivalentes = equivalentes
 
 def load_disciplinas() -> List:
     disciplinas = defaultdict()
@@ -74,7 +76,7 @@ def load_disciplinas() -> List:
                 disc_code = entry[0]
                 disc_CH = data['todos'][data['todos']['CODIGO'] == disc_code].values[0][8] if len(data['todos'][data['todos']['CODIGO'] == disc_code].values) > 0 else 0
                 disc_periodo = int(entry[1][1].split('\n')[0]) - 1
-                disciplinas[entry[0].split('\n')[0]] = Disciplina(ch = int(disc_CH), periodo = disc_periodo, nome = entry[1].split('\n')[0], codigo = entry[1].split('\n')[0], trilha = 'obrigatoria')
+                disciplinas[entry[0].split('\n')[0]] = Disciplina(ch = int(disc_CH), periodo = disc_periodo, nome = entry[0].split('\n')[0], codigo = entry[0].split('\n')[0], trilha = 'obrigatoria')
 
 
     with open('input/trilhas.txt', 'r') as file:
@@ -89,4 +91,17 @@ def load_disciplinas() -> List:
                     disc_periodo = 8
                     disciplinas[disc_code] = Disciplina(ch = int(disc_CH), periodo = disc_periodo, nome = item, codigo = item)
 
+    matriz = pd.read_csv('input/matriz_844.csv', sep=',')
+    for disc in matriz.itertuples():
+        #print(disc[3])
+        if(len(str(disc[16]).split(' ')) > 1):
+            print(disciplinas[disc[3]].codigo)
+            disciplinas[disc[3]].equivalentes = disc[16].split(' ')
+            print(disciplinas[disc[3]].equivalentes)
+
+    
+
     return disciplinas
+
+def get_matriz():
+    return pd.read_csv('input/matriz_844.csv', sep=',')
